529447bc5ccc766ae33f6120d2bd454effdfe313
inage <ikkainagero@gmail.com>
2013-12-18 02:32:26 +0000

    friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,30 @@
+# -*- coding: utf-8 -*-
 class UsersController < ApplicationController
-
+  before_action :signed_in_user, only:[:edit, :update]
+  before_action :correct_user, only:[:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
-
 
   def new
     @user = User.new
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+
+  
   def create
     @user = User.new(user_params)
     if @user.save
@@ -26,4 +42,13 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
+  def signed_in_user
+    redirect_to signin_url, notice: "サインインしてください" unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+  
 end
